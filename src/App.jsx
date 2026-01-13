e6595a1a6229a3515a69d9a47f53e9c1e46fa815
Kobayashi24730 <ajenteduplo000@gmail.com>
2026-01-13 20:28:46 -0300

    Implement routing in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import Bojo from "./components/Bojo";
-import Aro from "./components/Aro";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Body from "./components/Aro";
+import Product from "./components/Bojo";
 
-export default function App() {
+function App() {
   return (
-    <>
-      <Aro />	  
-      <Bojo />
-	
-    </>
+    <BrowserRouter>
+      <Routes>
+		<Route path="/" element={<Aro />} />
+        <Route path="/" element={<Bojo />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/cart" element={<h1>Carrinho</h1>} />
+        <Route path="/checkout" element={<h1>Checkout</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
+
+export default App;
