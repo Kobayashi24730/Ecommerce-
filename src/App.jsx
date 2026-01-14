c64914241d56d4df15b64fced25df6c2d6866d4c
Kobayashi24730 <ajenteduplo000@gmail.com>
2026-01-14 19:45:51 -0300

    Update routes and import for Cart component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Aro from "./components/Aro";     // Menu
+import Aro from "./components/Aro";     // Menu (Header)
 import Bojo from "./components/Bojo";   // Home
 import Product from "./components/Product";
+import Cart from "./components/Cart";
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Bojo />} />
         <Route path="/product/:id" element={<Product />} />
-        <Route path="/cart" element={<h1>Carrinho</h1>} />
+        <Route path="/cart" element={<Cart />} />
         <Route path="/checkout" element={<h1>Checkout</h1>} />
       </Routes>
 
